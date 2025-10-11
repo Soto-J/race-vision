a7401c8d28d33a9356adc2aa1fa9109a11702476
John Soto <soto.ja415@gmail.com>
2025-10-11 16:25:59 -0400

    Update VarBuffer struct to include offset parameter in constructor

--- src/sdk/var_buffer.rs
+++ src/sdk/var_buffer.rs
@@ -1,8 +1,10 @@
+#[repr(C)]
+#[derive(Debug, Clone)]
 pub struct VarBuffer {
     pub tick_count: i32, // 0 offset
     buff_offset: i32,    // 4 offset
 }
 
 impl VarBuffer {
-    pub fn new(shared_mem: *mut u8, buff_len: i32) {}
+    pub fn new(offset: usize, shared_mem: *mut u8, buff_len: i32) {}
 }
