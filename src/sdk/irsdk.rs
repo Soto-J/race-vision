385c18f9adbfce1d49cd9b63290e608b666818a5
John Soto <soto.ja415@gmail.com>
2025-10-16 04:55:36 -0400

    switch pc

--- src/sdk/irsdk.rs
+++ src/sdk/irsdk.rs
@@ -1,13 +1,17 @@
+use shared_memory::{Shmem, ShmemConf, ShmemError};
 use std::{collections::HashMap, ffi::c_void};
 
 use crate::sdk::{header::Header, var_buffer::VarBuffer, var_header::VarHeader};
+
+const MEM_MAP_FILE: &str = "Local\\IRSDKMemMapFileName";
+const MEM_MAP_FILE_SIZE: usize = 1164 * 1024;
 
 pub struct IRSDK {
     pub parse_yaml_async: bool,
     pub is_initialized: bool,
     pub last_session_info_update: u64,
     // memory/handle fields (Windows-specific will use raw pointers)
-    shared_mem: Option<Vec<u8>>,
+    shared_mem: Option<Shmem>,
     header: Option<Header>,
     data_valid_event: Option<*mut c_void>,
     // Variable header caching
@@ -48,5 +52,28 @@ impl IRSDK {
         }
     }
 
-    pub fn start_up(&self, test_file: Option<String>, dump_to: Option<String>) {}
+    pub fn start_up(
+        &self,
+        test_file: Option<String>,
+        dump_to: Option<String>,
+    ) -> Result<(), ShmemError> {
+        let shmem = ShmemConf::new()
+            .os_id(MEM_MAP_FILE)
+            .size(MEM_MAP_FILE_SIZE)
+            .open()?;
+
+        // Get a &[u8] view of the memory
+        let data = unsafe { shmem.as_slice() };
+
+        // Read first 4 bytes as i32
+        let tick_count_bytes = &data[0..4];
+        let tick_count = i32::from_le_bytes(tick_count_bytes.try_into().unwrap());
+
+        // Now you can read structured data directly from `data`
+        println!("First 16 bytes: {:?}", &data[..16]);
+
+        println!("tick_count = {}", tick_count);
+
+        Ok(())
+    }
 }
--- src/sdk/var_buffer.rs
+++ src/sdk/var_buffer.rs
@@ -3,8 +3,20 @@
 pub struct VarBuffer {
     pub tick_count: i32, // 0 offset
     buff_offset: i32,    // 4 offset
+
+    frozen_memory: Option<i32>,
+    pub is_memory_frozen: bool,
+    buff_len: i32,
 }
 
 impl VarBuffer {
-    pub fn new(offset: usize, shared_mem: *mut u8, buff_len: i32) {}
+    // pub fn new(&mut self, offset: usize, shared_mem: *mut u8, buff_len: i32) -> Self {
+    //     Self {
+    //         tick_count: (),
+    //         buff_offset: (),
+    //         frozen_memory: None,
+    //         is_memory_frozen: false,
+    //         buff_len,
+    //     }
+    // }
 }
